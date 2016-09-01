56969f931b08713dbb97d0c9b992ff91d201564c
Dekel zoaretz <dekelzoaretz@gmail.com>
2016-09-02 00:24:22 +0300

    removing unneeded variable of fields

--- api/src/App/Services/UsersService.php
+++ api/src/App/Services/UsersService.php
@@ -26,13 +26,6 @@ class UsersService extends BaseService
      */
     protected $token;
 
-    /**
-     * Default user fields to return
-     *
-     * @var string
-     */
-    private $userFields = 'user.id,user.first_name,user.last_name,user.mobile,user.email,user.unit,user.birth_date,user.membership_date,user.fb_profile_id,user.fb_url,user.about_me,user.external_web_site,user.image,user.video';
-
     public function __construct($db, $logInstance, $userId = false, $token = "")
     {
         parent::__construct($db);
